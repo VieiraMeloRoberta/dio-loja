c688d76e4f12427dca3b27e06d830992cbee5400
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2022-02-08 19:29:53 -0300

    Loja de Produtos

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Ar Condicionado",
+      price: 2000,
+      image: "./images/produtos/ArCondicionado.jpg",
+      id_categorys: 1,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Batedeira",
+      price: 350,
+      image: "./images/produtos/Batedeira.jpg",
+      id_categorys: 1,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Cadeira",
+      price: 230,
+      image: "./images/produtos/Cadeira.jpg",
+      id_categorys: 1,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "nacional",
+      fk_idcategorys: 2,
+      name_product: "Cafeteira",
+      price: 399,
+      image: "./images/produtos/Cafeteira.jpg",
+      id_categorys: 2,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "nacional",
+      fk_idcategorys: 2,
+      name_product: "Fritadeira",
+      price: 350,
+      image: "./images/produtos/Fritadeira.jpg",
+      id_categorys: 2,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "nacional",
+      fk_idcategorys: 2,
+      name_product: "Geladeira",
+      price: 2999,
+      image: "./images/produtos/Geladeira.jpg",
+      id_categorys: 2,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Nacional",
+      fk_idcategorys: 3,
+      name_product: "Impressora",
+      price: 2000,
+      image: "./images/produtos/Impressora.jpg",
+      id_categorys: 3,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "nacional",
+      fk_idcategorys: 3,
+      name_product: "Maquina de costura",
+      price: 450,
+      image: "./images/produtos/Maquinacostura.jpg",
+      id_categorys: 3,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "nacional",
+      fk_idcategorys: 4,
+      name_product: "Notebook Lenovo",
+      price: 3500,
+      image: "./images/produtos/NotebookLenovo.jpg",
+      id_categorys: 4,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "nacional",
+      fk_idcategorys: 4,
+      name_product: "Smart TV",
+      price: 3999,
+      image: "./images/produtos/SmartTV.jpg",
+      id_categorys: 4,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "nacional",
+      fk_idcategorys: 5,
+      name_product: "Sofa-cama",
+      price: 1800,
+      image: "./images/produtos/Sofa-cama.jpg",
+      id_categorys: 5,
+      name_categorys: "Loja",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "nacional",
+      fk_idcategorys: 5,
+      name_product: "Smartphone",
+      price: 1899,
+      image: "./images/produtos/Smartphone.jpg",
+      id_categorys: 5,
+      name_categorys: "Loja",
+      selected: false
+    },
+];
+
+export default productsList;
